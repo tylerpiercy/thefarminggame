b0a303b9dfe9abda0e5bcb713d6fd2b5dc46c448
Kimball Bighorse <kbighorse@yahoo.com>
2011-09-05 15:11:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thefarminggame::Application.config.secret_token = '001121dd89961e4c8dbe68204a129a9e52803ae2766cfbd0627d758a0c29f22df22257bfb9048dab3b5f133dc39946785cf9a8839872eafbb7d9db6c48dcb5eb'
